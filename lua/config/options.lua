89d507e18b94d26ebb6b0cfed435788660289fd3
Eli <78963198+niceEli@users.noreply.github.com>
2025-04-12 01:22:57 -0400

    feat: im posix now

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,7 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.shell = "nu"
+vim.opt.shell = "zsh"
 
 if vim.g.neovide then
   vim.o.guifont = "JetBrainsMono Nerd Font:h14"
