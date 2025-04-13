3b16b1bf920a84066bc31ba454ff8bbad85cc0a1
Eli <78963198+niceEli@users.noreply.github.com>
2025-04-13 14:28:36 -0400

    feat: update plugin configurations and remove unused plugins

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,58 @@
+return {
+  -- treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+        "rust",
+        "c",
+        "css",
+        "dockerfile",
+      },
+    },
+  },
+
+  -- Mason (AppLSP)
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "spellcheck",
+        "harper-ls",
+        "lua-language-server",
+        "stylua",
+        "css-lsp",
+        "html-lsp",
+        "typescript-language-server",
+        "deno",
+        "prettier",
+        "clangd",
+        "clang-format",
+        "rust-analyzer",
+        "bacon",
+        "bacon",
+        "bacon-ls",
+        "cpptools",
+        "chrome-debug-adapter",
+        "firefox-debug-adapter",
+        "js-debug-adapter",
+        "biome",
+        "codelldb",
+      },
+    },
+  },
+}
