3ae66a76acf83dd7a822f845f9a76092c1390e97
Eli <tape-infix.3m@icloud.com>
2025-09-02 12:06:41 -0400

    fix: remove error and make <A-/> silent

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,4 +7,5 @@ local map = vim.keymap.set
 map("t", "<C-x>", "<C-\\><C-n>")
 map("t", "<Esc>", "<C-\\><C-n>")
 map("i", "jk", "<Esc>")
-map("n", "<A-/>", ":noh<CR>")
+map("t", "jk", "<C-\\><C-n>")
+map("n", "<A-/>", ":noh<CR>", { silent = true })
