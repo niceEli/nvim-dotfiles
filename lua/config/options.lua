2b5e45a3d083e77d840d3e5cbc51e9c1d5d2c0e6
Eli <tape-infix.3m@icloud.com>
2025-09-01 21:49:10 -0400

    feat: updates

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,9 +1,13 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local opt = vim.opt
 
-vim.opt.shell = "zsh"
+
+opt.shell = "zsh"
 
 if vim.g.neovide then
   vim.o.guifont = "JetBrainsMono Nerd Font:h14"
 end
+
+opt.wrap = true
